64a5a46eed2777a82bf50e2f19def8f5135bb8db
joannahargote <48265997+joannahargote@users.noreply.github.com>
2019-03-14 11:02:23 +0800

    pill and shot values

--- src/Set/genData.java
+++ src/Set/genData.java
@@ -1,5 +1,12 @@
 package Set;
 
-public class genData extends Hound_And_Hostage {
+public class genData
+{
+//	GENERAL GAME DATA
+	
+	public static int
+	pillVal=15, //adds to hp, add 1/3 to strength
+	shotVal=10; // subtracts same value from hp, adds to strength
+	;
 
 }
