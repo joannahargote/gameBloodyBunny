4fc70527ced9277c41b94437535f25abc206db5c
joannahargote <48265997+joannahargote@users.noreply.github.com>
2019-03-14 11:36:34 +0800

    added methods

--- src/Set/npcData.java
+++ src/Set/npcData.java
@@ -32,12 +32,56 @@ public class npcData
 			npc.weapon=weapon;
 			npc.weaponType=weaponType;
 		}
+		
 		public void setInt(int hp, int strength, int pills, int shots)
 		{
 			npc.hp=hp;
 			npc.strength=strength;
 			npc.pills=pills;
 			npc.shots=shots;
+		}
+		
+		
+//		move the ff to mpcMethods if needed-----------------------------------------------
+		
+		public void getPill()
+		{	
+			IO.narration(npc.name+" took a pill.");
+			npc.pills--;
+			npc.hp+=genData.pillVal;
+			npc.strength+=genData.pillVal/3;
+			checkNPC();
+		}
+		
+		public void getInjection() 
+		{
+			IO.narration(npc.name+" injected himself.");
+			npc.shots--;
+			npc.hp-=genData.shotVal;
+			npc.strength+=genData.shotVal;
+			checkNPC();
+		}
+		
+		
+		public void checkNPC() //monitors NPC stats and values
+		{
+			if(npc.hp<1) //npc dies 
+			{
+				IO.narration( npc.name+" is dead.");
+				npc.alive=false;
+			}
+			else //is alive
+			{
+				if(npc.hp>100)
+				{
+					npc.hp=100; //adjust ceiling
+				}
+			}
+			
+			if(npc.strength>100)
+			{
+				npc.strength=100; //can't be too strong 
+			}
 		}
 		
 		
